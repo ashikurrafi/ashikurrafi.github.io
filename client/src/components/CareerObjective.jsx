9596bcbca70be6a313d893f0607fcb0e63a4520f
Ashikur Rafi <ashikurrafi@gmail.com>
2024-10-02 22:00:31 +0600

    static portfolio-1

--- client/src/components/CareerObjective.jsx
+++
@@ -1,9 +0,0 @@
-const CareerObjective = () => {
-  return (
-    <>
-      <h1>CareerObjective</h1>
-    </>
-  );
-};
-
-export default CareerObjective;
--- client/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
-};
-
-export default Navbar;
